6dfc9bac40889a60e7f8043dda6b44432125ab14
kevin murphy <kevinnealmurphy@gmail.com>
2020-04-30 19:26:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,11 +12,11 @@ end
 
 def reduce(source_array, starting_point = nil)
   if starting_point
-  new = starting_point
-  i = 0
+    new = starting_point
+    i = 0
   else
-  new = source_array[0]
-  i = 1
+    new = source_array[0]
+    i = 1
   end
   
   while i < source_array.length do
