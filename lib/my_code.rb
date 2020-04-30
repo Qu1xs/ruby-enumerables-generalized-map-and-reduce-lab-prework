2adc3cfc58873330569f9ac4b41b3375bdb745e1
kevin murphy <kevinnealmurphy@gmail.com>
2020-04-30 19:03:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,7 +14,7 @@ def reduce(source_array, starting_point = 0)
   new = []
   i = 0
   while i < source_array.length
-    yield(source_array[i])
+    new.push(yield(source_array[i]))
     i += 1
   end
   new
