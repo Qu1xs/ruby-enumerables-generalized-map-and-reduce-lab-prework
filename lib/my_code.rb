c0db5da1d86eaad3b1ab192499652399b7caa937
kevin murphy <kevinnealmurphy@gmail.com>
2020-04-30 19:23:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,22 +2,28 @@
 def map(source_array)
   new = []
   i = 0
-  while i < source_array.length
+  while i < source_array.length do
     new.push(yield(source_array[i]))
     i += 1
   end
-  new
+  return new
 end
 
 
 def reduce(source_array, starting_point = 0)
-  new = 0
+  if starting_point
+  new = starting_point
   i = 0
-  while i < source_array.length
-    yield(source_array[i] + starting_point)
+  else
+  new = source_array[0]
+  i = 1
+  end
+  
+  while i < source_array.length do
+    new = yield(new, source_array[i])
     i += 1
   end
-  new
+  return new
 end
 
 
@@ -33,8 +39,3 @@ end
 
 while i < source_array.length do
 new = yield(new, source_array[i])
-i += 1
-end
-return new
-
-end
