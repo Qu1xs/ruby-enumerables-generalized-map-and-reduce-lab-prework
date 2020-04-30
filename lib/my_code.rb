e9e893a4657c729cc465c305b271183adcf0c5be
kevin murphy <kevinnealmurphy@gmail.com>
2020-04-30 18:50:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,50 +14,8 @@ def reduce(source_array)
   new = []
   i = 0
   while i < source_array.length do
-    if !source_array[i]
+    if yield
       return false
-    end
-    i += 1
-  end
-  return new
-end
-
-my_own_reduce() do
-|n| if !source_array[i]
-  return false
-end
-
-def reduce_to_total(source_array, starting_point = 0)
-  i = 0
-  sum = 0
-  sum += starting_point
-  while i < source_array.length do
-    sum += source_array[i] 
-    i += 1
-  end
-  return sum
-end
-
-def reduce_to_all_true(source_array)
-  new = []
-  i = 0
-  while i < source_array.length do
-    if !source_array[i]
-      return false
-    end
-    i += 1
-  end
-  return new
-end
-
-def reduce_to_any_true(source_array)
-  new = []
-  i = 0
-  while i < source_array.length do
-    if !!source_array[i] 
-      new = true
-    else
-      new = false
     end
     i += 1
   end
